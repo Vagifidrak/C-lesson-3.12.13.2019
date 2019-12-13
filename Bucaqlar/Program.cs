122a578b8844e60d6b98ac031663d3ffb3a3db43
Vagifidrak <54859413+Vagifidrak@users.noreply.github.com>
2019-12-13 22:17:01 +0400

    Add files via upload

---
+++ Bucaqlar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Bucaqlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hesablamaq istediyin bucaqlari yaz");
+            int bucaq = Convert.ToInt32(Console.ReadLine());
+            dordBucaq drd = new dordBucaq(bucaq);
+            Console.WriteLine("Dordbucaqlinin sahesi:");
+            
+            {
+            }
+        }
+
+    }
+}
---
+++ Bucaqlar/class/buc.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Bucaqlar
+{
+    class coxBucaq
+    {
+                protected int teref; 
+
+            public coxBucaq (int teref)
+            {
+                this.teref = teref;
+            }
+        public virtual int Sahe()
+        {
+            return teref;
+        }
+    }
+    class dordBucaq : coxBucaq
+    {
+        public dordBucaq(int teref) : base(teref) {
+
+        }
+        public override int Sahe()
+        {
+            return teref * 4;
+        }
+    }
+
+    class ucBucaq : coxBucaq
+    {
+        public ucBucaq(int teref) : base(teref) { }
+
+        public override int Sahe()
+        {
+            return teref * 3;
+        }
+    }
+}
+
